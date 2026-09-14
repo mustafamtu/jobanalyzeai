97b316a61bf60fc506326251666bfd3fb6ef6c7f
mustafamtu <mustafamtu3458@gmail.com>
2026-09-14 17:18:42 +0300

    added: pdf loader.

--- jobanalyzeai/arayuz.py
+++ jobanalyzeai/arayuz.py
@@ -1,15 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Hoşgeldin!")
 st.markdown(
     """ 
-    Başvurduğun ilan için ne kadar uyumlu olduğunu görmek \n
-    Eksik yönlerini iyileştirmek,\n
-    Artı yönlerini öğrenmek,\n
-    Ve hangi ilanların zaman kaybı olduğunu görmek mi istiyorsun?\n
+    Başvurduğun ilan için ne kadar uyumlu olduğunu görmek,
+    Eksik yönlerini iyileştirmek,
+    Artı yönlerini öğrenmek,
+    Ve hangi ilanların zaman kaybı olduğunu görmek mi istiyorsun?
     Job Analyze AI bunun için var. 
     """
 )
 
-if st.button("Analiz Et"):
-    st.balloons()
+uploaded_files = st.file_uploader(
+    "CV'ni Yükle (PDF)", accept_multiple_files=True, type="pdf"
+)
+for uploaded_file in uploaded_files:
+    cv = pd.read_pdf(uploaded_file)
+    st.write(cv)
